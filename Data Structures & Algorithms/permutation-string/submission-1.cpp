f86372c10b381847117a7f35a5450100759bcdd8
Brian Jacobs <51355307+bjacobs42@users.noreply.github.com>
2026-06-01 00:11:03 +0200

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    bool checkInclusion(string s1, string s2)
+    {
+        std::array<int, 26> s1_count = {};
+        std::array<int, 26> s2_count = {};
+        for (char c : s1)
+            ++s1_count[c - 'a'];
+        
+        int left = 0;
+        for (int right = 0; right < s2.size(); ++right)
+        {
+            int c_index = s2[right] - 'a';
+
+            ++s2_count[c_index];
+            while (s2_count[c_index] > s1_count[c_index])
+            {
+                --s2_count[s2[left] - 'a'];
+                ++left;
+            }
+            if (right - left + 1 == s1.size())
+                return (true);
+        }
+        return (false);
+    }
+};
