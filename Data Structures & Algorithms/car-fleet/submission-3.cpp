522c722061a50188d3c3ad9dd6bf026af1538ea7
Brian Jacobs <51355307+bjacobs42@users.noreply.github.com>
2026-06-08 16:36:40 +0200

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed)
+    {
+        std::vector<std::pair<int, int>> cars;
+
+        cars.reserve(position.size());
+        for (int i = 0; i < position.size(); ++i)
+            cars.emplace_back(position[i], speed[i]);
+
+        std::sort(cars.rbegin(), cars.rend());
+
+        int i = 0;
+        int fleetCount = 0;
+        while (i < position.size())
+        {
+            int j = i + 1;
+            double max_time = (double)(target - cars[i].first) / cars[i].second;
+
+            while (j < position.size())
+            {
+                double time = (double)(target - cars[j].first) / cars[j].second;
+                if (time > max_time)
+                    break;
+                ++j;
+            }
+            i = j;
+            ++fleetCount;
+        }
+        return (fleetCount);
+    }
+};
